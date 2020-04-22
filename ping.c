58bc89171b6c53c0c3aaa02b06c2ece6adb3427b
yiransii <yiransi@umich.edu>
2020-04-22 01:15:43 -0400

    added more to ping ctor

--- ping.c
+++ ping.c
@@ -53,20 +53,54 @@ void start(struct Ping *p) {
     
 }
 
+
+
 //ctor for Ping
-struct Ping ping(struct sockaddr_in *sour_addr, struct sockaddr_in * dest_addr) {
+struct Ping ping(char* hostname) {
     struct Ping p;
     
     // init data
-    p.sour_addr = *sour_addr;
+//    p.sour_addr = *sour_addr;
     p.datalen = 56;
     p.nsend = 0;
     p.nreceived = 0;
     
-    // assign member functions
+    // assign member function pointers
     p.send_packet = &sending;
     p.recv_packet = &listening;
     p.start = &start;
+    
+    // init socket
+//    struct sockaddr_in * sour_addr;
+//    struct sockaddr_in * dest_addr;
+    // get sour_addr
+    // get dest_addr from argv[1]
+    struct hostent *host;
+    struct protoent *protocol;
+    unsigned long in_addr = 0;
+    //int sockfd;
+    int buff_sz = 1024; // tmp 1024
+    // get host && set socket
+    protocol = getprotobyname("icmp");
+    if (protocol == NULL) {
+        perror("failed to get protocol");
+        exit(1);
+    }
+    p.sockfd = socket(AF_INET, SOCK_RAW, protocol->p_proto);
+    if (p.sockfd < 0) {
+        perror("failed to init socket");
+        exit(1);
+    }
+    int res = setsockopt(p.sockfd, SOL_SOCKET, SO_RCVBUF, &buff_sz, sizeof(buff_sz));
+    if (res < 0) {
+        perror("failed to set socket");
+        exit(1);
+    }
+    memset(&p.dest_addr, 0, sizeof(struct sockaddr_in)); // set mem to 0
+    p.dest_addr.sin_family = AF_INET;
+    
+    // TODO: if input is IP, get hostname by IP
+    // TODO: set dest_addr
     
     return p;
 }
@@ -80,13 +114,8 @@ int main(int argc, char **argv) {
         perror("not enough argument");
         return -1;
     }
-    
-    struct sockaddr_in * sour_addr;
-    struct sockaddr_in * dest_addr;
-    // get sour_addr
-    // get dest_addr from argv[1]
-    
-    struct Ping p = ping(sour_addr, dest_addr);
+
+    struct Ping p = ping(argv[1]);
     p.start(&p);
     
     return 0;
